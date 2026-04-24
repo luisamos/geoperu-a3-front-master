8b6dca56a047e81ea2fca1d568e7a02c2b7cf169
Luis Amos <luisamos7@gmail.com>
2026-04-23 22:42:40 -0500

    feat: add RENADESPPLE_URL to endpoints.js

--- endpoints.js
+++ endpoints.js
@@ -2,10 +2,12 @@ const MAP_BASE_URL = process.env.MAP_BASE_URL;
 const MAP_URL = process.env.MAP_URL;
 const REPORTE_URL =  process.env.REPORTE_URL;
 const REPORTE_CEM_URL = process.env.REPORTE_CEM_URL;
+const RENADESPPLE_URL = process.env.RENADESPPLE_URL;
 // export
 export {
     MAP_URL,
     REPORTE_URL,
     REPORTE_CEM_URL,
-    MAP_BASE_URL
+    MAP_BASE_URL,
+    RENADESPPLE_URL
 }
